f6a772f41c2287c5a4dc6165f84db51c2fbf1a8f
Cooktypeset <tovpyhalitva@gmail.com>
2023-08-08 23:46:38 +0300

    upgr

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
+import style from './Notification.module.css';
 
-export const Notification = ({ message }) => {
-    return (
-        <p>{message}</p>
-    )
+const Notification = ({ message }) => {
+  return <p className={style.message}>{message}</p>;
 };
 
 Notification.propTypes = {
-    message: PropTypes.string.isRequired
-}
+  message: PropTypes.string.isRequired,
+};
+
+export default Notification;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import PropTypes from "prop-types";
-import css from './Section.module.css';
+import PropTypes from 'prop-types';
 
-export const Section = ({ title, children }) => {
-    return (
-        <section className={css.section}>
-            <h2>{title}</h2>
-            {children}
-        </section>
-    )
+const Section = ({ title, children }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
 };
+
 Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.element,
-}
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+export default Section;
